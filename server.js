8138af3a219480ad02a0fd109e334cf84837633e
ItzSanuka <ItzSanuka@outlook.com>
2024-08-23 15:24:27 +1000

    You can make a new request

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+let collection;
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        const database = client.db("BuildUp");
+        collection = database.collection("requests");
+    } catch(error) {
+        console.error("Error when trying to connect to DB:", error.message);
+        await connectToDatabase();
+    }
+}
+
+const app = express();
+const port = 5000;
+
+app.get("/requests/create", async function(req, res) {
+    await connectToDatabase();
+
+    try {
+        await collection.insertOne(req.body);
+        await client.close();
+
+        res.status(200).send("Request added to database.");
+    } catch(error) {
+        console.error("Error when creating new request:", error.message);
+
+        res.status(500).send("Request failed to be added to database.");
+    }
+});
+
+app.listen(port, function() {
+    console.log(`Listening on ${port}`);
+});
